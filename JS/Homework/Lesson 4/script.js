c1bb1df79c4502c40a3e931badaa3f7cb1c7f862
jiraiya106 <jiraiya106@gmail.com>
2021-03-11 17:04:44 +0300

    Lesson 4

--- JS/Homework/Lesson 4/script.js
+++ JS/Homework/Lesson 4/script.js
@@ -1,103 +1,103 @@
 "use strict"
-{
-    let question = prompt("Каково «официальное» название JavaScript?");
-    if (question === "ECMAScript") {
-        console.log("Верно!");
-    } else {
-        console.log("Не знаете? «ECMAScript»!");
-    }
-}
+// {
+//     let question = prompt("Каково «официальное» название JavaScript?");
+//     if (question === "ECMAScript") {
+//         console.log("Верно!");
+//     } else {
+//         console.log("Не знаете? «ECMAScript»!");
+//     }
+// }
 
-{
-    let NoName = +prompt("Введите число!!!!");
+// {
+//     let NoName = +prompt("Введите число!!!!");
     
-    if (NoName > 0) {
-            console.log ("+");
-        } else if (NoName < 0) {
-            console.log ("-");
-        } else {
-            if (NoName === 0) {
-                console.log ("0");
-            } else
-            console.log ("Что за хренЬ???");
-        }
+//     if (NoName > 0) {
+//             console.log ("+");
+//         } else if (NoName < 0) {
+//             console.log ("-");
+//         } else {
+//             if (NoName === 0) {
+//                 console.log ("0");
+//             } else
+//             console.log ("Что за хренЬ???");
+//         }
     
-}
+// }
 
-{
-    let date = new Date();
-    let  h= date.getHours();
-    if (h < 11) {
-        console.log ("Доброе утро");
-    } else if (h > 11 && h < 14) {
-        console.log ("бодрый день");
-    } else {
-        console.log ("и т.д");
-    }
-}
+// {
+//     let date = new Date();
+//     let  h= date.getHours();
+//     if (h < 11) {
+//         console.log ("Доброе утро");
+//     } else if (h > 11 && h < 14) {
+//         console.log ("бодрый день");
+//     } else {
+//         console.log ("и т.д");
+//     }
+// }
 
-{
-    let FirstNumber = +prompt("Введите первое число");
-    let SecondNumber = +prompt("Введите второе число");
+// {
+//     let FirstNumber = +prompt("Введите первое число");
+//     let SecondNumber = +prompt("Введите второе число");
 
-    if (FirstNumber > SecondNumber) {
-        console.log ("1");
-    } else if (FirstNumber < SecondNumber) {
-        console.log ("2");
-    } else {
-        if (FirstNumber === SecondNumber) {
-            console.log ("0");
-        } else
-        console.log ("Что за хренЬ???");
-    }
-}
+//     if (FirstNumber > SecondNumber) {
+//         console.log ("1");
+//     } else if (FirstNumber < SecondNumber) {
+//         console.log ("2");
+//     } else {
+//         if (FirstNumber === SecondNumber) {
+//             console.log ("0");
+//         } else
+//         console.log ("Что за хренЬ???");
+//     }
+// }
 
-{
-    let FirstNumber = +prompt("Введите первое число");
-    let SecondNumber = +prompt("Введите второе число");
-    let ThreeNumber = +prompt("Введите третье число");
+// {
+//     let FirstNumber = +prompt("Введите первое число");
+//     let SecondNumber = +prompt("Введите второе число");
+//     let ThreeNumber = +prompt("Введите третье число");
 
-    if (FirstNumber < SecondNumber) {
-        if(SecondNumber < ThreeNumber) {
-            console.log("Третье");
-        } else if (SecondNumber === ThreeNumber) {
-            console.log("Второе = Третье");
-        } else {
-            console.log("Второе");
-        }
-    } else if (FirstNumber < ThreeNumber){
-        if (FirstNumber === ThreeNumber) {
-            console.log("1 = 3");
-        } else {
-            console.log("3");
-        }    
-    } else {
-        if (FirstNumber > ThreeNumber){
-            console.log("1");
-        } else if (FirstNumber == SecondNumber == ThreeNumber) 
-            console.log("Они равны");
-        else {
-            console.log("Что за хрень?");
-        }
-    }
-}
+//     if (FirstNumber < SecondNumber) {
+//         if(SecondNumber < ThreeNumber) {
+//             console.log("Третье");
+//         } else if (SecondNumber === ThreeNumber) {
+//             console.log("Второе = Третье");
+//         } else {
+//             console.log("Второе");
+//         }
+//     } else if (FirstNumber < ThreeNumber){
+//         if (FirstNumber === ThreeNumber) {
+//             console.log("1 = 3");
+//         } else {
+//             console.log("3");
+//         }    
+//     } else {
+//         if (FirstNumber > ThreeNumber){
+//             console.log("1");
+//         } else if (FirstNumber == SecondNumber == ThreeNumber) 
+//             console.log("Они равны");
+//         else {
+//             console.log("Что за хрень?");
+//         }
+//     }
+// }
 
-{
-    let Row = +prompt("Введите n");
-    let Colums = +prompt("Введите m");
-    let Total = +prompt("Введите к")
+// {
+//     let Row = +prompt("Введите n");
+//     let Colums = +prompt("Введите m");
+//     let Total = +prompt("Введите к")
 
-    if (Number.isInteger(Row) && Number.isInteger(Colums) && Number.isInteger(Total)) {
-    if (Total === Row) {
-        console.log("Можно по n");
-    } else if (Total === Colums) {
-        console.log("Можно по m"); 
-    } else {
-        console.log("Нельзя");
-    }
+//     if (Number.isInteger(Row) && Number.isInteger(Colums) && Number.isInteger(Total)) {
+//     if (Total === Row) {
+//         console.log("Можно по n");
+//     } else if (Total === Colums) {
+//         console.log("Можно по m"); 
+//     } else {
+//         console.log("Нельзя");
+//     }
 
-    } else {
-        console.log("Что за хрень?");
-    }
-}
+//     } else {
+//         console.log("Что за хрень?");
+//     }
+// }
 
--- JS/Homework/Lesson 4/script2.js
+++ JS/Homework/Lesson 4/script2.js
@@ -1,73 +1,155 @@
 "use strict"
-{
-    let Number = +prompt("Введите номер месяца?");
+// Задание 1
+// Дан номер месяца (1 – январь, 2 – февраль, ...). Вывести название соответствующего времени года («зима», «весна» и т. д.).
 
-    switch(Number){
-        case 1: 
-        case 2: console.log("Зима"); break;
-        case 3:
-        case 4:    
-        case 5: console.log("Весна"); break;
-        case 6:
-        case 7:
-        case 8: console.log("Лето"); break;
-        case 9:
-        case 10:
-        case 11: console.log("Осень"); break;
-        case 12: console.log("Зима"); break;
-        default: console.log("ЧТО ЗА ХРЕНЬ?????");
-    }
-}
+// {
+//     let Number = +prompt("Введите номер месяца?");
+
+//     switch(Number){
+//         case 1: 
+//         case 2: console.log("Зима"); break;
+//         case 3:
+//         case 4:    
+//         case 5: console.log("Весна"); break;
+//         case 6:
+//         case 7:
+//         case 8: console.log("Лето"); break;
+//         case 9:
+//         case 10:
+//         case 11: console.log("Осень"); break;
+//         case 12: console.log("Зима"); break;
+//         default: console.log("ЧТО ЗА ХРЕНЬ?????");
+//     }
+// }
+
+//Задание 2
+//Дано целое число в диапазоне от 0 до 9. Вывести строку – название соотвествующей цифры на русском языке (0 – «ноль», 1 – «один», 2 – «два», ...)
+
+// {
+//     let Number = +prompt("Введите число");
+
+//     switch(Number){
+//         case 1: console.log("один"); break;
+//         case 2: console.log("два"); break;
+//         case 3:console.log("три"); break;
+//         case 4: console.log("четыре"); break;   
+//         case 5: console.log("пять"); break;
+//         case 6:console.log("шесть"); break;
+//         case 7:console.log("семь"); break;
+//         case 8: console.log("восемь"); break;
+//         case 9:console.log("девять"); break;
+//         case 10:console.log("ноль"); break;
+//         default: console.log("ЧТО ЗА ХРЕНЬ?????");
+//     }
+// }
+
+// Задание 3
+// Узнать который сейчас час в Javascript можно следующим образом:
+// let date = new Date();
+// let  h= date.getHours();
+// Составьте скрипт, который спрашивает имя пользователя и приветствует его  по-разному,  в зависимости от времени суток. (Доброе утро, бодрый день и т.д)
+
+// {
+//     let date = new Date();
+//     let  h= date.getHours();
+    
+//     switch(h){
+//         case 0: console.log("Полночь"); break;
+//         case 1: 
+//         case 2: 
+//         case 3:
+//         case 4:console.log("Уже ночь. Иди сапать!!!"); break;    
+//         case 5: 
+//         case 6:
+//         case 7:
+//         case 8: 
+//         case 9:
+//         case 10:
+//         case 11: console.log("Доброе утро"); break;
+//         case 12: console.log("Полдень"); break;
+//         case 13:
+//         case 14:
+//         case 15: 
+//         case 16:console.log("Добрый день"); break;
+//         case 17:
+//         case 18: 
+//         case 19:
+//         case 20:
+//         case 21: console.log("Добрый вечер"); break;
+//         case 22: console.log("Уже ночь. Иди сапать!!!"); break;
+//         case 23:
+        
+//         default: console.log("ЧТО ЗА ХРЕНЬ?????");
+//     }
+
+// }
+
+// Задание 4
+// Дано целое число в диапазоне от 1 до 5. 
+//Вывести строку – словесное описание соответствующей оценки 
+//(1 – «плохо», 2 – «неудовлетворительно», 3 – «удовлетворительно», 4 – «хорошо», 5 – «отлично»)
+
+// {
+//     let Number = +prompt("Введите число");
+
+//     switch(Number) {
+//         case 1: console.log("плохо"); break;
+//         case 2: console.log("неудовлетворительно"); break;
+//         case 3: console.log("удовлетворительно"); break;
+//         case 4: console.log("хорошо"); break;
+//         case 5: console.log("отлично"); break;
+//         default: console.log("Число больше возможного"); break;
+//     }
+// }
+
+// Задание 5
+// Единицы длины пронумерованы следующим образом: 
+// 1 – дециметр, 2 – километр, 3 – метр, 4 – миллиметр, 5 – сантиметр. 
+// Дан номер единицы длины N и длина отрезка L в этих единицах (вещественное число). 
+// Вывести длину данного отрезка в метрах
+
+// {
+//     let LeghtUnits = +prompt("Введите номер единицы измерения");
+//     let SegmentLeght = +prompt("Введите длинну отрезка");
+
+//     switch(LeghtUnits) {
+//         case 1: console.log(`${SegmentLeght} дециметр`); break;
+//         case 2: console.log(`${SegmentLeght} километр`); break;
+//         case 3: console.log(`${SegmentLeght} метр`); break;
+//         case 4: console.log(`${SegmentLeght} миллиметр`); break;
+//         case 5: console.log(`${SegmentLeght} сантиметр`); break;
+//         default: console.log("Нет таких единиц");
+//     }
+// }
+
+// Задание 6
+// Составить программу, которая печатает номера дней в месяце, 
+// если вводится день недели. Считаем, что 1–е число месяца – понедельник, 
+// в месяце 30 дней.
+
+// {
+//     let NumberDays = +prompt("Введите число месяца");
+//            switch(true) {
+//             case NumberDays === 1 || NumberDays === 8 || NumberDays === 15 || NumberDays === 22 || NumberDays === 29: console.log("Понедельник"); break;
+//             case NumberDays === 2 || NumberDays === 9 || NumberDays === 16 || NumberDays === 23 || NumberDays === 30: console.log("Вторник"); break;
+//             case NumberDays === 3 || NumberDays === 10 || NumberDays === 17 || NumberDays === 24: console.log("Среда"); break;
+//             case NumberDays === 4 || NumberDays === 11 || NumberDays === 18 || NumberDays === 25: console.log("Четверг"); break;
+//             case NumberDays === 5 || NumberDays === 12 || NumberDays === 19 || NumberDays === 26: console.log("Пятница"); break;
+//             case NumberDays === 6 || NumberDays === 13 || NumberDays === 20 || NumberDays === 27: console.log("Суббота"); break;
+//             case NumberDays === 7 || NumberDays === 14 || NumberDays === 21 || NumberDays === 28: console.log("Воскресенье"); break;
+//             default: console.log("Нет такой даты"); 
+//         } 
+// }
+
+// Задание 7* (По желанию)
+// Дано целое число в диапазоне от 100 до 999. 
+// Вывести строку – словесное описание данного числа, например: 
+// 256 – «двести пятьдесят шесть», 814 – «восемьсот четырнадцать».
 
 {
     let Number = +prompt("Введите число");
-
-    switch(Number){
-        case 1: console.log("один"); break;
-        case 2: console.log("два"); break;
-        case 3:console.log("три"); break;
-        case 4: console.log("четыре"); break;   
-        case 5: console.log("пять"); break;
-        case 6:console.log("шесть"); break;
-        case 7:console.log("семь"); break;
-        case 8: console.log("восемь"); break;
-        case 9:console.log("девять"); break;
-        case 10:console.log("ноль"); break;
-        default: console.log("ЧТО ЗА ХРЕНЬ?????");
-    }
-}
-
-{
-    let date = new Date();
-    let  h= date.getHours();
-    
-    switch(h){
-        case 0: console.log("Полночь"); break;
-        case 1: 
-        case 2: 
-        case 3:
-        case 4:console.log("Уже ночь. Иди сапать!!!"); break;    
-        case 5: 
-        case 6:
-        case 7:
-        case 8: 
-        case 9:
-        case 10:
-        case 11: console.log("Доброе утро"); break;
-        case 12: console.log("Полдень"); break;
-        case 13:
-        case 14:
-        case 15: 
-        case 16:console.log("Добрый день"); break;
-        case 17:
-        case 18: 
-        case 19:
-        case 20:
-        case 21: console.log("Добрый вечер"); break;
-        case 22: console.log("Уже ночь. Иди сапать!!!"); break;
-        case 23:
-        
-        default: console.log("ЧТО ЗА ХРЕНЬ?????");
-    }
-
+    console.log(Math.trunc(Number/100))
+    console.log(Number%100)
+    console.log(Math.trunc(Number%100/10))
+    console.log(Number%10)
 }
