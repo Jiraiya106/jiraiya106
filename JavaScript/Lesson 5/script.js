0e0a8b159a805c4937b00c5ab652cceb6a22acdf
Jiraiya106 <jiraiya106@gmail.com>
2021-03-13 12:55:21 +0300

    lesson 8

---
+++ JavaScript/Lesson 5/script.js
@@ -0,0 +1,22 @@
+"use strict"
+// let number = 10 * Math.random();
+// let a = Math.trunc(number);
+
+// do {
+//     let b = +prompt("Введите")
+//     if (b > a) {
+//         console.log("Многовато");
+//     } 
+//     if (b < a) {
+//         console.log("Маловато");
+//     }
+// } while (b != a)
+
+//     console.log("Угадали");
+let n = prompt("Введите");
+let m = 0;
+for(let i = 0; n = null; i++) {
+    m = m + n;
+    n = +prompt("Введите");
+}
+console.log(`m=${m}`);
